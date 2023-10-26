61a594b779390e467f77bc6b047594a2f0f96b02
Khurshida-Meem <meemrumi59@gmail.com>
2023-10-26 20:33:47 +0600

    updations

--- src/components/tasks-landing/index.js
+++ src/components/tasks-landing/index.js
@@ -1,30 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
+import { Droppable } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd";
 
 const Landing = () => {
+  const [tasks, setTasks] = useState([]);
+  const [showingData, setShowingData] = useState([]);
 
-    const [tasks, setTasks] = useState([]);
-    const [showingData, setShowingData] = useState([]);
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    if (result.destination.index === result.source.index) return;
+    const data = reorder(tasks, result.source.index, result.destination.index);
+    //store reordered state.
+    setTasks(data);
+    setShowingData(data);
+  };
 
-    const reorder = (list, startIndex, endIndex) => {
-        const result = Array.from(list);
-        const [removed] = result.splice(startIndex, 1);
-        result.splice(endIndex, 0, removed);
-        return result;
-      };
-      const onDragEnd = (result) => {
-        if (!result.destination) return;
-        if (result.destination.index === result.source.index) return;
-        const data = reorder(tasks, result.source.index, result.destination.index);
-        //store reordered state.
-        setTasks(data);
-        setShowingData(data);
-      };
-
-    return (
+  return (
+    <div>
+      <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
         <div>
-            
+          <Droppable droppableId="AllQuestion">
+            {(queDropProvided) => (
+              <div
+                ref={queDropProvided.innerRef}
+                {...queDropProvided.droppableProps}
+              >
+                {showingData?.map((item, index) => {
+                  return (
+                    <Draggable
+                      key={index}
+                      draggableId={index.toString()}
+                      index={index}
+                    >
+                      {(queProvided, index) => (
+                        <div
+                          ref={queProvided.innerRef}
+                          {...queProvided.draggableProps}
+                          {...queProvided.dragHandleProps}
+                          index={index}
+                        >
+                          {/* <DragIndicatorIcon sx={{ fontSize: "12px" }} /> */}
+                          <p>Drag me</p>
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+
+                {queDropProvided.placeholder}
+              </div>
+            )}
+          </Droppable>
         </div>
-    );
+      </DragDropContext>
+    </div>
+  );
 };
 
 export default Landing;
